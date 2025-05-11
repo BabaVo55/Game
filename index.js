7f30e7e936097ca412db85146d929defdd8b65ca
BabaVo55 <ethemcode@outlook.com>
2025-05-11 02:16:50 +0100

    refactoring - abstraction complete

--- index.js
+++ index.js
@@ -6,13 +6,14 @@ const width = canvas.width = 1024;
 const height = canvas.height = 576;
 
 c.fillRect(0, 0, width, height);
-const gravity = .2;
+const gravity = .7;
 
 class Sprite {
     constructor({position, velocity, size}){
         this.position = position;
         this.size = size
         this.velocity = velocity;
+        this.lastKey;
         
     }
 
@@ -31,6 +32,7 @@ class Sprite {
             //✅ Problem with ==:
             //Rarely will this.position.y + this.size.height + velocity.y be exactly equal to canvas.height.
             this.velocity.y = 0
+            // this.position.y = canvas.height
             //🧠 1. Why + this.velocity.y in the condition?
             //So using + velocity.y is a predictive collision check — it helps prevent 
             // the object from going slightly past the floor before stopping, which avoids 
@@ -76,9 +78,6 @@ const enemy = new Sprite(
     }
 )
 
-let lastPressed;
-let lastPressedEnemy;
-
 const keys = {
     a: {
         pressed: false
@@ -100,14 +99,23 @@ const keys = {
     },
 }
 
-function movement(button1, button2, pressed, who){
-    if (keys.button1.pressed && pressed === 'a'){
-        who.velocity.x = -1;
-    } 
-    else if (keys.button2.pressed && pressed === 'd'){
-        who.velocity.x = 1;
-    } 
+
+// function movement(char, key, button1, button2){
+//     if (keys.key.pressed && char.lastKey === button1){
+//         char.velocity.x = -2
+//     } else if (keys.key.pressed && char.lastKey === button2){
+//         char.velocity.x = 2
+//     }
+// }
+
+function movement(char, keysObj, key1, key2){
+    if (keysObj[key1].pressed && char.lastKey === key1){
+        char.velocity.x = -2
+    } else if (keysObj[key2].pressed && char.lastKey === key2){
+        char.velocity.x = 2
+    }
 }
+
 
 function animate(){
     window.requestAnimationFrame(animate)
@@ -117,7 +125,28 @@ function animate(){
     enemy.update()
     
     player.velocity.x = 0
-    movement(a,)
+    enemy.velocity.x = 0
+
+    
+    movement(player, keys, 'a', 'd')
+    movement(enemy, keys, 'arrowLeft', 'arrowRight')
+
+    // movement(player, {a,d}, 'a', 'd')
+    // movement(enemy, {arrowRight, arrowLeft}, 'ArrowRight', 'ArrowLeft')
+    
+    // if (keys.a.pressed && player.lastKey === 'a'){
+    //     player.velocity.x = -2
+    // } else if (keys.d.pressed && player.lastKey === 'd'){
+    //     player.velocity.x = 2
+    // }
+ 
+    // if (keys.arrowLeft.pressed && enemy.lastKey === 'arrowLeft'){
+    //     enemy.velocity.x = -2
+    // } else if (keys.arrowRight.pressed && enemy.lastKey === 'arrowRight'){
+    //     enemy.velocity.x = 2
+    // }
+
+
 
 }
 
@@ -126,26 +155,26 @@ document.addEventListener('keydown', (e) => {
     switch(e.key){
         case 'd':
             keys.d.pressed = true;
-            lastPressed = 'd'
+            player.lastKey = 'd'
         break
         case 'a':
             keys.a.pressed = true;
-            lastPressed = 'a'
+            player.lastKey = 'a'
         break
         case 'w':
-            player.velocity.y = -10
+            player.velocity.y = -20
             keys.w.pressed = true;
         break
-        case 'arrowRight':
+        case 'ArrowRight':
             keys.arrowRight.pressed = true;
-            lastPressedEnemy = 'arrowRight'
+            enemy.lastKey = 'arrowRight'
         break
-        case 'arrowLeft':
+        case 'ArrowLeft':
             keys.arrowLeft.pressed = true;
-            lastPressedEnemy = 'arrowLeft'
+            enemy.lastKey = 'arrowLeft'
         break
-        case 'arrowUp':
-            enemy.velocity.y = -10
+        case 'ArrowUp':
+            enemy.velocity.y = -20
             keys.arrowUp.pressed = true;
         break
 
@@ -165,13 +194,13 @@ document.addEventListener('keyup', (e) => {
             player.velocity.y = 2; 
             keys.w.pressed = false;
         break
-        case 'arrowRight':
+        case 'ArrowRight':
             keys.arrowRight.pressed = false;
         break
-        case 'arrowLeft':
+        case 'ArrowLeft':
             keys.arrowLeft.pressed = false;
         break
-        case 'arrowUp':
+        case 'ArrowUp':
             enemy.velocity.y = 2; 
             keys.arrowUp.pressed = false;
         break
